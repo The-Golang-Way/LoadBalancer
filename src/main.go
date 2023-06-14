e324d4691d33d476e5df8e8499a670b3624cf7dd
Yehdar <p.radhey06@gmail.com>
2023-06-14 14:00:01 -0400

    defined the server struct

--- src/main.go
+++ src/main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"net/http/httputil"
+)
 
-func main() {
-	fmt.Printf("Hello World! \n")
+type simpleServer struct {
+	addr string
+	proxy *httputil.ReverseProxy
 }
