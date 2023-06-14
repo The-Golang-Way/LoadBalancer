53d83f1ed70428a2e7048a183d1f2852380a2028
Yehdar <p.radhey06@gmail.com>
2023-06-14 14:05:11 -0400

    constructor to initialize address and proxy in server

--- src/main.go
+++ src/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"net/http/httputil"
+	"net/url"
 )
 
 type simpleServer struct {
 	addr string
 	proxy *httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleServer {
+	serverUrl, bruhWhatDoIDoHere := url.Parse(addr)
+
+	return &simpleServer{
+		addr: addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+	}
 }
