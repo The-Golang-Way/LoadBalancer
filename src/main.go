7dc981474e7f0896c36743c645c74a380a8adc70
Yehdar <p.radhey06@gmail.com>
2023-06-15 11:13:07 -0400

    forgot to add main function to start testing

--- src/main.go
+++ src/main.go
@@ -12,12 +12,13 @@ type Server interface{
 	isAlive() bool
 	Serve(rw http.ResponseWriter, r *http.Request)
 }
-
+// creates a simple server on a port
 type SimpleServer struct {
 	addr string
 	proxy *httputil.ReverseProxy
 }
 
+// think of this as the constructor class from java 
 func newSimpleServer(addr string) *SimpleServer {
 	serverUrl, err := url.Parse(addr)
 	handleErr(err)
@@ -28,12 +29,14 @@ func newSimpleServer(addr string) *SimpleServer {
 	}
 }
 
+// creates the loadbalancer
 type LoadBalancer struct {
 	port 			string
 	roundRobinCount int
 	servers 		[]Server
 }
 
+// think of this as the constructor class from java 
 func newLoadBalancer(port string, servers []Server) *LoadBalancer{
 	return &LoadBalancer{
 		port: 			 port,
@@ -42,8 +45,18 @@ func newLoadBalancer(port string, servers []Server) *LoadBalancer{
 	}
 }
 
+// similar to catch-and-throw assertations but super duper ghetto
 func handleErr(err error){
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 	}
+}
+
+func main() {
+	servers := []Server{
+		newSimpleServer("https://github.com/yehdar"),
+		newSimpleServer("https://github.com/fakeshell"),
+		newSimpleServer("https://github.com/huaanth"),
+	}
+	lb := newLoadBalancer("8000", servers)
 }
